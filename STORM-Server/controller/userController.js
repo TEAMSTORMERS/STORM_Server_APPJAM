97ba1d2bbaa916b85fcb1fc9a80fc3c282f8a136
Say-young <seyoung990525@gmail.com>
2020-07-08 15:30:47 +0900

    이미지 저장 완료

--- STORM-Server/controller/userController.js
+++ STORM-Server/controller/userController.js
@@ -6,11 +6,12 @@ const UserModel = require('../dao/user');
 const user = {
     signup : async (req, res) => {
         //1. request body에서 값을 읽어온다.
-        const {user_name, user_token_kakao, user_token_google, user_img} = req.body;
-        const user_img = req.files;
+        const {user_name, user_token_kakao, user_token_google} = req.body;
+        const user_img = req.file.location;
       
         //예외처리1 : parameter 체크 - 하나라도 null이나 undefined가 들어올 경우
         if(!user_name || (!user_token_kakao && !user_token_google) || user_img === undefined){
+          console.log("여기?");
           res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, resMessage.NULL_VALUE)); //400은 요청이 잘못됐다는 오류 메세지
           return;
         }
@@ -31,6 +32,7 @@ const user = {
       
         //2. 새로운 User를 등록한다.
         const userIdx = await UserModel.signup(user_name, user_token_kakao, user_token_google, user_img);
+        console.log("여기??");
         if(userIdx === -1) {
           return res.status(statusCode.DB_ERROR).send(util.fail(statusCode.DB_ERROR, resMessage.DB_ERROR));
         }
