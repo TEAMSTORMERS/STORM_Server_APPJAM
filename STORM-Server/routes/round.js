16b9f3058fddb873aac9cd0a226cd96b2c5fb9a2
Say-young <seyoung990525@gmail.com>
2020-07-17 00:54:09 +0900

    round 수정 완료

--- STORM-Server/routes/round.js
+++ STORM-Server/routes/round.js
@@ -6,7 +6,7 @@ router.get("/count/:project_idx", roundController.roundCount);
 router.post("/setting", roundController.roundSetting);
 router.get("/info/:project_idx", roundController.roundInfo);
 router.post("/enter", roundController.roundEnter);
-router.delete("/leave", roundController.roundLeave);
+//router.delete("/leave", roundController.roundLeave);
 router.get("/memberList/:round_idx", roundController.roundParticipant);
 router.get("/cardList/:project_idx/:round_idx", roundController.roundCardList);
 router.get("/roundFinalInfo/:project_idx", roundController.roundFinalInfo);
