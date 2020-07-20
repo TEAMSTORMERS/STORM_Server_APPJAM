42de042fdbd83b604fdb9d1989f099a1560f3ecf
Choong <cndqjacndqja@naver.com>
2020-07-20 21:13:06 +0900

    STORM 1차 완료

--- STORM-Server/app.js
+++ STORM-Server/app.js
@@ -75,7 +75,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 
-// catch 404 and forward to error handler
+// catch 404 and forward to error handler 
 app.use(function (req, res, next) {
   next(createError(404));
 });
